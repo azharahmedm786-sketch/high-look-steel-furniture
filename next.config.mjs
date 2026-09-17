65eec299af45e7e385cb3aa846d1e279fbd56805
Azhar Ahamed <azharahmedm786@gmail.com>
2026-09-17 20:32:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/high-look-steel-furniture",
-  assetPrefix: "/high-look-steel-furniture/",
   images: {
     unoptimized: true,
   },
